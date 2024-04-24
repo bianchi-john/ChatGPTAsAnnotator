27f0b67f09a3e2ea4936c48c6e032df068aa7262
bianchi-john <john.lenci.bianchi@gmail.com>
2024-04-24 16:24:29 +0200

    Small update

--- modules/deleteNotCoerent.py
+++ modules/deleteNotCoerent.py
@@ -1,4 +1,14 @@
+import os
 def deleteNotCoerent(df, name):
+
+
+    # Definisci il percorso della cartella 'Other'
+    folder_path = 'Other'
+
+    # Verifica se la cartella 'Other' esiste, altrimenti creala
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
     # Definisci i campi di interesse
     required_fields = ['Q1.1', 'Q1.2', 'Q1.4', 'Q1.5', 'Q1.6', 'Q1.7', 'Q2.8', 'Q2.9.1', 'Q2.10']
 
@@ -15,15 +25,17 @@ def deleteNotCoerent(df, name):
     # Rimuovi le righe con tutti valori -1 dalle righe con almeno un valore -1
     one_negative_one_rows = one_or_more_negative_one_rows.loc[~all_negative_one]
 
-    # Salva le righe con tutti valori -1 in un file CSV
+        # Salva le righe con tutti valori -1 in un file CSV
     if not all_negative_one_rows.empty:
-        all_negative_one_rows.to_csv(str(name) + '_tutti_i_valori_sono_-1.csv', index=False)
+        file_path_all_negative_one = os.path.join(folder_path, str(name) + '_tutti_i_valori_sono_-1.csv')
+        all_negative_one_rows.to_csv(file_path_all_negative_one, index=False)
         number = all_negative_one_rows.shape[0]
         print(f"Numero di righe con tutti valori impostati a -1 (mancanti) per {name}: {number}")
 
-    # Salva le righe con almeno un valore impostato a -1
+    # Salva le righe con almeno un valore impostato a -1 nella cartella 'Other'
     if not one_negative_one_rows.empty:
-        one_negative_one_rows.to_csv(str(name) + '_almeno_un_valore_-1.csv', index=False)
+        file_path_one_negative_one = os.path.join(folder_path, str(name) + '_almeno_un_valore_-1.csv')
+        one_negative_one_rows.to_csv(file_path_one_negative_one, index=False)
         number = one_negative_one_rows.shape[0]
         print(f"Numero di righe con almeno un valore mancante nei campi obbligatori per {name}: {number}")
     else:
